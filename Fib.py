96eff6a09158d5b683d166c90e2575a5a74ad674
bdemin <42113340+bdemin@users.noreply.github.com>
2019-05-18 16:17:26 +0300

    Added lut_fib implementation
    Additional implementations are not done yet.

--- Fib.py
+++ Fib.py
@@ -16,7 +16,32 @@ def better_fib(n, mem = {0: 0, 1: 1}):
         mem[n] = (better_fib(n-1, mem) + better_fib(n-2, mem))    
     return mem[n]
 
-for i in range(10):
-    print(better_fib(i))
+# for i in range(10):
+    # print(better_fib(i))
 
-print(better_fib(100))
+# print(better_fib(100))
+
+def lut_fib(n):
+    if n < 2:
+        return n
+    lut = [0, 1]
+    for i in range(2, n + 1):
+        lut.append(lut[i-1] + lut[i-2])
+    return lut[n]
+
+# for i in range(10):
+    # print(lut_fib(i))
+
+
+def even_better_fib(n, mem = [0, 1]):
+    if len(mem) < n:
+        mem.append(even_better_fib(n-1, mem) + even_better_fib(n-2, mem))
+    return mem[n]
+
+# a=2
+# print(even_better_fib(a))
+
+import numpy as np 
+
+def test(n, mem = np.zeros(n)):
+    if n
